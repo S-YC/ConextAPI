617b40c1b7b54be1a9a790f2ddd8c795b0acfab3
S-YC <s9292909@gmail.com>
2022-04-03 18:12:03 +0900

    Consumer -> useContext, static contextType  코드변경

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ConextAPI 개념 이해하기
 
+## 블로그 주소
+
+[티스토리 블로그](https://sycdev.tistory.com/)
+
 ## 동적 Context 사용
 
 ConextAPI 사용 색상 변경 구현
@@ -9,3 +13,4 @@ ConextAPI 사용 색상 변경 구현
 | 색상 변경구현       | 색상 선택구현             |
 | ------------------- | ------------------------- |
 | OnClick 이벤트 사용 | onContextMenu 이벤트 사용 |
+| useContext 사용     | static contextType 사용   |
--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 // Consumer 컴포넌트를 통해 색상 조회
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
